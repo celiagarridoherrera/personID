d64ad82d0a70762edf108c010a91db770475f542
Celia Garrido <celiagarridoherrera@gmail.com>
2025-01-30 23:51:05 +0100

    first commit

---
+++ src/main/java/dev/celia/person/App.java
@@ -0,0 +1,14 @@
+package dev.celia.person;
+
+public final class App {
+    private App() {
+    }
+
+    /**
+     * 
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        PersonID person1 = new PersonID("Celia", "Garrido", "12345678A", 1990);
+    }
+}
